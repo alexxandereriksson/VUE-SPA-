53095cd0d71246dec7d53af60657ae1fbd1ea69b
Alexander Eriksson <Alexxandereriksson@hotmail.com>
2023-02-10 01:33:17 +0100

    2

---
+++ vue-spa1/src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">Home</RouterLink>
+    </div>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped></style>
